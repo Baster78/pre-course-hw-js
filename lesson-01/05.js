990cc8cfff433a2c4de5cd89c3236c77503d2543
Tash <chermoreau@gmail.com>
2023-07-20 16:10:31 +0300

    test 5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -3,15 +3,12 @@ let rule =
 
 switch (true) {
   case rule.length > 25:
-    rule.length > 25;
     console.log('У меня 100 пудов все получится');
     break;
   case rule.length < 25:
-    rule.length < 25;
-    console.log('Все таки нет правил без исключения');
+    console.log('"Все таки нет правил без исключения"');
     break;
-  case rule.length > 25:
-    rule.length == 25;
-    console.log('50 на 50');
+  case rule.length == 25:
+    console.log('"50 на 50"');
     break;
 }
