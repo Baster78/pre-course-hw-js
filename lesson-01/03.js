e649be6932882f8e088cef99459a6f77458a8ba2
Tash <chermoreau@gmail.com>
2023-07-20 13:45:09 +0300

    test 3 - 8

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,27 +1,6 @@
-// let number = 1;
+let number = 1;
 
-// let a = number++;
-
-// console.log(a);
-
-// let b = ++number;
-
-// console.log(b);
-
-// let c = number--;
-
-// console.log(c);
-
-// let d = --number;
-
-// console.log(d);
-
-// console.log('----');
-
-// number = number++;
-
-// console.log(number);
-
-// number = ++number;
-
-// console.log(number);
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
