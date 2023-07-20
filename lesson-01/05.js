a737b8426028be6a11b336194279f007ee13fd05
Tash <chermoreau@gmail.com>
2023-07-20 16:29:09 +0300

    t5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -4,6 +4,11 @@ let rule =
 switch (true) {
   case rule.length < 25:
     console.log('"Все таки нет правил без исключения"');
+    break;
   case rule.length > 25:
     console.log('У меня 100 пудов все получится');
+    break;
+  case rule.length == 25:
+    console.log('У меня 100 пудов все получится');
+    break;
 }
