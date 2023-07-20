4c190f08a1b1bdd72e9ea77b7ae6e0fd367becb7
Tash <chermoreau@gmail.com>
2023-07-20 15:05:11 +0300

    test 5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,14 +1,17 @@
 let rule =
   'Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом';
 
-let a = 25;
+let a = '25';
 let b = rule.length;
 
 switch (true) {
   case a > b:
     console.log('Все таки нет правил без исключения');
+    break;
   case a < b:
     console.log('У меня 100 пудов все получится');
+    break;
   case a == b:
     console.log('50 на 50');
+    break;
 }
