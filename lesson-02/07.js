4e30085028cbd87aa329784da7709676a6e341dd
Tash <chermoreau@gmail.com>
2023-07-25 13:29:00 +0300

    t5

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -7,24 +7,24 @@ let passportMarried = {
   },
 };
 
-// function copyMarried(passportMarried) {
-//   let copiedPrep = {
-//     ...passportMarried,
-//     married: true,
-//     address: { ...passportMarried.address },
-//   };
-//   return copiedPrep;
-// }
+function copyMarried(passportMarried) {
+  let copiedPrep = {
+    ...passportMarried,
+    married: true,
+    address: { ...passportMarried.address },
+  };
+  return copiedPrep;
+}
 
-// let copiedPrep = copyMarried(passportMarried);
-
-// console.log(passportMarried);
-
-// console.log(copiedPrep);
-
-let passportMarriedTrue = { ...passportMarried };
-
-passportMarriedTrue.married = true;
+let copiedPrep = copyMarried(passportMarried);
 
 console.log(passportMarried);
-console.log(passportMarriedTrue);
+
+console.log(copiedPrep);
+
+// let passportMarriedTrue = { ...passportMarried };
+
+// passportMarriedTrue.married = true;
+
+// console.log(passportMarried);
+// console.log(passportMarriedTrue);
