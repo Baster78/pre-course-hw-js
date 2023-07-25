269921df875780a44da6439bc7da80780a106bda
Tash <chermoreau@gmail.com>
2023-07-25 14:36:22 +0300

    l1 5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,15 +1,14 @@
 let rule =
   'Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом';
 
-const checkStringLength = (rule) => {
-  switch (true) {
-    case rule.length > 25:
-      console.log('У меня 100 пудов все получится');
-    case rule.length < 25:
-      console.log('Все таки нет правил без исключения');
-    case rule.length === 25:
-      console.log('50 на 50');
-  }
-};
-
-checkStringLength(rule);
+switch (true) {
+  case rule.length > 25:
+    console.log('У меня 100 пудов все получится');
+    break;
+  case rule.length < 25:
+    console.log('Все таки нет правил без исключения');
+    break;
+  case rule.length === 25:
+    console.log('50 на 50');
+    break;
+}
