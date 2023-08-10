e86379389ca6350133305a27b64e54346f267529
Tash <chermoreau@gmail.com>
2023-08-10 10:09:50 +0300

    w3 l2

---
+++ lesson-03/02.js
@@ -0,0 +1,9 @@
+let words = ['яблоко', 'банан', 'апельсин', 'манго', 'киви'];
+
+let word = words.indexOf('апельсин');
+
+if (word === words.indexOf('апельсин')) {
+  console.log('Ура! нашел');
+} else {
+  console.log('Придется поискать в другом магазине...');
+}
