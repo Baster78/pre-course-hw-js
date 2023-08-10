7bb70bf1b42b92fe27fcb079dfde3aa6965293e9
Tash <chermoreau@gmail.com>
2023-08-10 10:42:01 +0300

    w3 l2

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,6 +1,7 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
 
-if (words.indexOf("апельсин") === 2) {
+let orangeYes = words.indexOf("апельсин");
+if (orangeYes != -1) {
   console.log("Ура! нашел");
 } else {
   console.log("Придется поискать в другом магазине...");
